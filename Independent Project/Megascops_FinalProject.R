aeda1690ec11c8159fbde3aedb54b9d9105ccf5c
tokyonymph <emmagreenm21@gmail.com>
2024-04-20 21:10:23 -0400

    More on Final Project
    More files relating to my final project have been added.

--- Independent Project/Megascops_FinalProject.R
+++ Independent Project/Megascops_FinalProject.R
@@ -3,6 +3,7 @@ library(Biostrings)
 library(msa)
 library(seqinr)
 library(ape)
+library(phangorn)
 
 # We will first do the MSA for Megascops asio. Start by creating vectors for each subspecies.
 m_a_asio <- readDNAStringSet("C:/Users/emmag/OneDrive/School Work/2024-2025/SPRING 2024/ST GR Bioinformatics - BSC 6932/Bioinformatics_Spring2024/Independent Project/Screech Owl Cytb Data/Megascops asio/m_asio_asio_cytb.fasta")
@@ -18,3 +19,20 @@ m_asio_msa <- msa(m_asio_seqs, method=c("ClustalW"))
 alignment_m_asio <- msaConvert(fm_asio_msa, type="phangorn::phyDat")
 write.phyDat(alignment_m_asio, "meg_asio_msa.fasta", format="fasta")
 
+# Repeat same process for other species; next, Megascops kennicottii.
+m_k_aikeni <- readDNAStringSet("C:/Users/emmag/OneDrive/School Work/2024-2025/SPRING 2024/ST GR Bioinformatics - BSC 6932/Bioinformatics_Spring2024/Independent Project/Screech Owl Cytb Data/Megascops kennicottii/m kennicottii aikeni.fasta")
+m_k_benderi <- readDNAStringSet("C:/Users/emmag/OneDrive/School Work/2024-2025/SPRING 2024/ST GR Bioinformatics - BSC 6932/Bioinformatics_Spring2024/Independent Project/Screech Owl Cytb Data/Megascops kennicottii/m kennicottii benderi.fasta")
+m_k_kennicottii <- readDNAStringSet("C:/Users/emmag/OneDrive/School Work/2024-2025/SPRING 2024/ST GR Bioinformatics - BSC 6932/Bioinformatics_Spring2024/Independent Project/Screech Owl Cytb Data/Megascops kennicottii/m kennicottii kennicottii.fasta")
+m_k_mcfarlandii <- readDNAStringSet("C:/Users/emmag/OneDrive/School Work/2024-2025/SPRING 2024/ST GR Bioinformatics - BSC 6932/Bioinformatics_Spring2024/Independent Project/Screech Owl Cytb Data/Megascops kennicottii/m kennicottii mcfarlandii.fasta")
+m_k_suttoni <- readDNAStringSet("C:/Users/emmag/OneDrive/School Work/2024-2025/SPRING 2024/ST GR Bioinformatics - BSC 6932/Bioinformatics_Spring2024/Independent Project/Screech Owl Cytb Data/Megascops kennicottii/m kennicottii suttoni.fasta")
+
+m_kenn_seqs <- c(m_k_aikeni, m_k_benderi, m_k_kennicottii, m_k_mcfarlandii, m_k_suttoni)
+m_kenn_msa <- msa(m_kenn_seqs, method=c("ClustalW"))
+align_m_kenn <- msaConvert(m_kenn_msa, type="phangorn::phyDat")
+write.phyDat(align_m_kenn, "meg_kenn_msa.fasta", format="fasta")
+
+m_wc_set_path <- "C:/Users/emmag/OneDrive/School Work/2024-2025/SPRING 2024/ST GR Bioinformatics - BSC 6932/Bioinformatics_Spring2024/Independent Project/Screech Owl Cytb Data/Megascops watsoni + choliba/Megascops_watsonii_and_choliba_cytb.fas"
+m_wach_set <- readDNAStringSet(m_wc_set_path, format = "fasta")
+m_wach_msa <- msa(m_wach_set, method = c("ClustalW"))
+m_wach_align <- msaConvert(m_wach_msa, type="phangorn::phyDat")
+write.phyDat(m_wach_align, "m_wat_cho_msa.fasta", format="fasta")
